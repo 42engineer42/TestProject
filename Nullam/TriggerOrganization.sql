ecb0ad9fcb391b2e833c927a5af3c7ec5f1174c4
johannabv <99389409+johannabv@users.noreply.github.com>
2022-08-24 23:32:56 +0300

    Added triggers to delete participants of events that are deleted

---
+++ Nullam/TriggerOrganization.sql
@@ -0,0 +1,7 @@
+﻿CREATE TRIGGER DeleteParticipantTriggerOrganization
+	ON [Nullam].[Events]
+	AFTER DELETE
+	AS
+	DELETE FROM [Nullam].[Organizations]
+	WHERE EventId NOT IN (SELECT Id FROM [Nullam].[Events])
+	GO
---
+++ Nullam/TriggerPerson.sql
@@ -0,0 +1,7 @@
+﻿CREATE TRIGGER DeleteParticipantTriggePerson
+	ON [Nullam].[Events]
+	AFTER DELETE
+	AS
+	DELETE FROM [Nullam].[Persons]
+	WHERE EventId NOT IN (SELECT Id FROM [Nullam].[Events])
+	GO
