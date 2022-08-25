78ae1c9b01537c91b8c41570f3ae65afd68de0e1
johannabv <99389409+johannabv@users.noreply.github.com>
2022-08-25 22:08:15 +0300

    Added validation for event date

--- Facade/Party/EventView.cs
+++ Facade/Party/EventView.cs
@@ -7,10 +7,16 @@ using Nullam.Domain.Party;
 namespace Nullam.Facade.Party {
     public sealed class EventView : BaseView {
         [DisplayName("Ürituse nimi"), Required] public string? Name { get; set; }
-        [DisplayName("Toimumisaeg"), Required] public DateTime? Date { get; set; }
+        [DisplayName("Toimumisaeg"), Required, EventDateValidation] public DateTime? Date { get; set; }
         [DisplayName("Koht"), Required] public string? Place { get; set; }
     }
     public sealed class EventViewFactory : BaseViewFactory<EventView, Event, EventData> {
         protected override Event ToEntity(EventData d) => new(d);
+    }
+    public class EventDateValidation : ValidationAttribute {
+        public override bool IsValid(object value) {
+            string dateAsString = value.ToString().Replace(".", "/");
+            return DateTime.Parse(dateAsString) >= DateTime.Now;
+        }
     }
 }
